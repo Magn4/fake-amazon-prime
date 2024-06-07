ba157eabc9da6d2ab00f04531ba52c6ef5364b42
Nuronoria <jrwieloch@gmail.com>
2024-06-07 21:49:21 +0200

    aktueller stand

--- login/src/main/java/module-info.java
+++ login/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.login {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.login to javafx.fxml;
     exports com.mycompany.login;
